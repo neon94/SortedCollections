23e96ec0b7436c52f9162fa78d39c106f9ff3765
Dawid Jaloza <dawidjaloza@gmail.com>
2020-05-25 15:31:34 +0200

    Completed main.java class + tests or working. Commit before challenge.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,28 +1,41 @@
 package com.example;
 
+import java.util.Map;
+
 public class Main {
     private static StockList stockList = new StockList();
 
     public static void main(String[] args) {
-        StockItem temp = new StockItem("bread", 0.85, 100);
+        StockItem temp = new StockItem("bread", 0.86, 100);
         stockList.addStock(temp);
 
         temp = new StockItem("cake", 1.10, 7);
         stockList.addStock(temp);
+
         temp = new StockItem("car", 12.50, 2);
         stockList.addStock(temp);
+
         temp = new StockItem("chair", 62.00, 10);
         stockList.addStock(temp);
+
         temp = new StockItem("cup", 0.50, 200);
         stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
         temp = new StockItem("door", 72.95, 4);
         stockList.addStock(temp);
+
         temp = new StockItem("juice", 2.50, 36);
         stockList.addStock(temp);
+
         temp = new StockItem("phone", 96.99, 35);
         stockList.addStock(temp);
+
         temp = new StockItem("towel", 2.40, 80);
         stockList.addStock(temp);
+
         temp = new StockItem("vase", 8.76, 40);
         stockList.addStock(temp);
 
@@ -31,5 +44,52 @@ public class Main {
         for(String s : stockList.Items().keySet()) {
             System.out.println(s);
         }
+
+        Basket dawidsBasket = new Basket("Dawid");
+        sellItem(dawidsBasket, "car", 1);
+        System.out.println(dawidsBasket);
+
+        sellItem(dawidsBasket, "car", 1);
+        System.out.println(dawidsBasket);
+
+        if(sellItem(dawidsBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars in shop");
+        }
+        sellItem(dawidsBasket, "car", 1);
+        sellItem(dawidsBasket, "sponge", 1);
+        System.out.println(dawidsBasket);
+
+        sellItem(dawidsBasket, "juice", 4);
+        sellItem(dawidsBasket, "cup", 12);
+        sellItem(dawidsBasket, "bread", 1);
+        System.out.println(dawidsBasket);
+
+        System.out.println(stockList);
+
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+        stockList.Items().get("car").adjustStock(2000);
+        stockList.get("car").adjustStock(-1000);
+        System.out.println(stockList);
+        for(Map.Entry<String, Double> price: stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+
     }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+//        retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if(stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+
 }
